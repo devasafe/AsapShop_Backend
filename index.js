ec152e981e487121890e7d32c2f6d0683858c03c
Asafe <contatoasapdev@gmail.com>
2025-11-12 03:02:07 -0300

    cors

--- index.js
+++ index.js
@@ -33,7 +33,11 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 app.use(
   cors({
-    origin: [FRONTEND_URL, 'http://localhost:3000'],
+    origin: [
+      FRONTEND_URL,
+      'http://localhost:3000',
+      'https://asap-shop-frontend.vercel.app'
+    ],
     credentials: true,
   })
 );
